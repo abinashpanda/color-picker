b40c2fc6472b4c972236328ce69db4508ba8561f
Abinash Panda <abinash@prodios.com>
2023-10-13 19:32:53 +0530

    feat(gradient): add selector for gradient type and angle

--- src/app/_components/gradient-picker-popover.tsx
+++ src/app/_components/gradient-picker-popover.tsx
@@ -21,6 +21,16 @@ const GRADIENT_TYPE_OPTIONS: Record<GradientValue['gradientType'], string> = {
   radial: 'Radial',
 }
 const GRADIENT_TYPES_ORDER: GradientValue['gradientType'][] = ['linear', 'radial']
+const ANGLES = [
+  { value: '0', label: 'To top' },
+  { value: '45', label: 'To top right' },
+  { value: '90', label: 'To right' },
+  { value: '135', label: 'To bottom right' },
+  { value: '180', label: 'To bottom' },
+  { value: '225', label: 'To bottom left' },
+  { value: '270', label: 'To left' },
+  { value: '315', label: 'To top left' },
+]
 
 export default function GradientPickerPopover({ value, onChange, className, style }: GradientPickerPopoverProps) {
   invariant(value.type === 'gradient', 'GradientPickerPopover must be used with a gradient value')
@@ -99,11 +109,41 @@ export default function GradientPickerPopover({ value, onChange, className, styl
     [value, handleActiveIndexChange],
   )
 
-  console.log(value)
+  const handleGradientTypeChange = useCallback(
+    (selectedValue: string) => {
+      if (selectedValue === 'radial') {
+        onChange({
+          ...value,
+          gradientType: 'radial',
+        })
+      } else if (selectedValue === 'linear') {
+        const angle = value.gradientType === 'linear' ? value.angle : 90
+        onChange({
+          ...value,
+          gradientType: 'linear',
+          angle,
+        })
+      }
+    },
+    [onChange, value],
+  )
+
+  const handleGradientAngleChange = useCallback(
+    (selectedAngle: string) => {
+      if (value.gradientType === 'linear') {
+        const angle = Number.parseInt(selectedAngle, 10)
+        onChange({
+          ...value,
+          angle,
+        })
+      }
+    },
+    [onChange, value],
+  )
 
   return (
     <div className={cn('w-[200px] space-y-2', className)} style={style}>
-      <Select value={value.gradientType}>
+      <Select value={value.gradientType} onValueChange={handleGradientTypeChange}>
         <SelectTrigger>
           <SelectValue placeholder="Gradient Type"></SelectValue>
         </SelectTrigger>
@@ -117,6 +157,22 @@ export default function GradientPickerPopover({ value, onChange, className, styl
           })}
         </SelectContent>
       </Select>
+      {value.gradientType === 'linear' ? (
+        <Select value={value.angle.toString()} onValueChange={handleGradientAngleChange}>
+          <SelectTrigger>
+            <SelectValue placeholder="Select Gradient Angle"></SelectValue>
+          </SelectTrigger>
+          <SelectContent>
+            {ANGLES.map((angle) => {
+              return (
+                <SelectItem key={angle.value} value={angle.value.toString()}>
+                  {angle.label}
+                </SelectItem>
+              )
+            })}
+          </SelectContent>
+        </Select>
+      ) : null}
       <GradientStopsSelector
         value={value}
         activeIndex={activeIndex}
